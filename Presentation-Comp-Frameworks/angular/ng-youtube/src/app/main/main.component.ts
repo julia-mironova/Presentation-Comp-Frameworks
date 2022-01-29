464c743622db644ac58da6a342e8bbdb58900e0e
Yuliya Mironova <rabkevich15@gmail.com>
2022-01-29 17:07:22 +0100

    feat: angular counter

--- Presentation-Comp-Frameworks/angular/ng-youtube/src/app/main/main.component.ts
+++ Presentation-Comp-Frameworks/angular/ng-youtube/src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  writeNum: number = 0;
+  plusBtn() {
+    this.writeNum++;
+  }
+  minusBtn() {
+    this.writeNum--;
+  }
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
